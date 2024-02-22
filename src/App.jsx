d8c57405d43dff72596090128865bb0d90407e22
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2024-02-22 22:25:33 +0530

    Added Auto class sorter for tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="font-black text-3xl underline">
+    <div className="text-3xl font-black underline">
       <p className="font-spaceGrotesk">
         Vite + TailwindCSS Template, with 3 fonts
       </p>
