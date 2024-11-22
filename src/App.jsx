979072dccd3ab40f927d8d01ded8dfbf74273f61
HarikrishnanYPR1707 <hyprgd2002@gmail.com>
2024-11-22 22:21:08 +0530

    Mansonry Layout is implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,43 @@
 import "./App.css";
 
 function App() {
+  const data = [
+    {
+      name: "Harikrishnan",
+      desc: "Generating random paragraphs can be an excellent way for writers to get their creative flow going at the beginning of the day. The writer has no idea what topic the random paragraph will be about when it appears. This forces the writer to use creativity to complete one of three common writing challenges. The writer can use the paragraph as the first one of a short story and build upon it. A second option is to use the random paragraph somewhere in a short story they create. The third option is to have the random paragraph be the ending paragraph in a short story. No matter which of these challenges is undertaken, the writer is forced to use creativity to incorporate the paragraph into their writing.",
+    },
+    {
+      name: "Harikrishnan",
+      desc: "Generating random paragraphs can be an excellent way for writers to get their creative flow going at the beginning of the day. The writer has no idea what topic the random paragraph will be about when it appears. This forces the writer to use creativity to complete one of three common writing challenges. The writer can use the paragraph as the first one of a short story and build upon it. A second option is to use the random paragraph somewhere in a short story they create. The third option is to have the random paragraph be the ending paragraph in a short story. No matter which of these challenges is undertaken, the writer is forced to use creativity to incorporate the paragraph into their writing. Generating random paragraphs can be an excellent way for writers to get their creative flow going at the beginning of the day. The writer has no idea what topic the random paragraph will be about when it appears. This forces the writer to use creativity to complete one of three common writing challenges. The writer can use the paragraph as the first one of a short story and build upon it. A second option is to use the random paragraph somewhere in a short story they create. The third option is to have the random paragraph be the ending paragraph in a short story. No matter which of these challenges is undertaken, the writer is forced to use creativity to incorporate the paragraph into their writing.Generating random paragraphs can be an excellent way for writers to get their creative flow going at the beginning of the day. The writer has no idea what topic the random paragraph will be about when it appears. This forces the writer to use creativity to complete one of three common writing challenges. The writer can use the paragraph as the first one of a shrandom pll be about when it appears. This forces the writer to use creativity to complete one of three common writing challenges. The writer can use the paragraph as the first one of a short story and build upon it. A second option is to use the random paragraph somewhere in a short story they create. The third option is to have the random paragraph be the ending paragraph in a short story. No matter which of these challenges is undertaken, the writer is forced to use creativity to incorporate the paragraph into their writing.Generating random paragraphs can be an excellent way for writers to get their creative flow going at the beginning of the day. The writer has no idea what .ort story and build upon it. A second option is to use the random paragraph somewhere in a short story they create. The third option is to have the random paragraph be the ending paragraph in a short story..",
+    },
+    {
+      name: "Harikrishnan",
+      desc: "Generating random paragraphs can be an excellent way for writers to get their creative flow going at the beginning of the day. The writer has no idea what topic the random paragraph will be about when it appears. This forces the writer to use creativity to complete one of three common writing challenges. The writer can use the paragraph as the first one of a short story and build upon it. A second option is to use the random paragraph somewhere in a short story they create. The third option is to have the random paragraph be the ",
+    },
+    {
+      name: "Harikrishnan",
+      desc: "Generating random paragraphs can be an excellent way for writers to get their creative flow going at the beginning of the day. The writer has no idea what topic the random paragraph will be about when it appears. This forces the writer to use creativity to complete one of three common writing challenges. The writer can use the paragraph as the first one of a short story and build upon it. A second option is to use the random paragraph somewhere in a short story they create. The third option is to have the random paragraph be the ending paragraph in a short story. No matter which of these challenges is undertaken, the writer is forced to use creativity to incorporate the paragraph into their writing.Generating random paragraphs can be an excellent way for writers to get their creative flow going at the beginning of the day. The writer has no idea what topic the random paragraph will be about when it appears. This forces the writer to use creativity to complete one of three common writing challenges. The writer can use the paragraph as the first one of a short story and build upon it. A second option is to use the random paragraph somewhere in a short story they create. The third option is to have the random paragraph be the ending paragraph in a short story. No matter which of these challenges is undertaken, the writer is forced to use creativity to incorporate the paragraph into their writing.Generating random paragraphs can be an excellent way for writers to get their creative flow going at the beginning of the random paragraph somewhere in a short story they create. The third option is to have the random paragraph be the ending paragraph in a short story. No matter which of these challenges is undertaken, the writer is forced to use creativity to incorporate the paragraph into their writing.Generating random paragraphs can be an excellent way for writers to get their creative flow going at the beginning of the",
+    },
+    {
+      name: "Harikrishnan",
+      desc: "Generating random paragraphs can be an excellent way for writers to get their creative flow going at the beginning of the day. The writer has no idea what topic the random paragraph will be about when it appears. This forces the writer to use creativity to complete one of three common writing challenges. The writer can use the paragraph as the first one of a short story and build upon it. A second option is to use the random paragraph somewhere in a short story they create. The third option is to have the random paragraph be the ending paragraph in a short story. No matter which of these challenges is undertaken, the writer is forced to use creativity to incorporate the paragraph into their writing.Generating random paragraphs can be an excellent way for writers to get their creative flow going at the beginning of the day. The writer has no idea what topic the random paragraph will be about when it appears. This forces the writer to use creativity to complete one of three common writing challenges. The writer can use the paragraph as the first one of a short story and build upon it. A second option is to use the random pll be about when it appears. This forces the writer to use creativity to complete one of three common writing challenges. The writer can use the paragraph as the first one of a short story and build upon it. A second option is to use the random paragraph somewhere in a short story they create. The third option is to have the random paragraph be the ending paragraph in a short story. No matter which of these challenges is undertaken, the writer is forced to use creativity to incorporate the paragraph into their writing.Generating random paragraphs can be an excellent way for writers to get their creative flow going at the beginning of the day. The writer has no idea what .",
+    },
+  ];
   return (
-    <div className="text-3xl font-black underline">
-      <p className="font-spaceGrotesk">
-        Vite + TailwindCSS Template, with 3 fonts
-      </p>
-      <p className="font-familjenGrotesk">
-        Vite + TailwindCSS Template, with 3 fonts pardeshi
-      </p>
-      <p className="font-spaceMono">
-        Vite + TailwindCSS Template, with 3 fonts
-      </p>
+    <div className="border-2 border-purple-500 bg-[#1e1e1e] p-[100px]">
+      {/* ==================== */}
+      {/* The main properties are "columns-2 space-y-5" */}
+      <div className="mx-auto w-[1000px] columns-2 space-y-5 border border-red-500">
+        {data.map((item, idx) => (
+          // Here the "break-inside-avoid" property is important, it says how the column space should break
+          <div className="flex h-fit w-full break-inside-avoid flex-col gap-3 rounded-lg border bg-white p-10">
+            <h3 className="font-familjenGrotesk text-5xl">{item.name}</h3>
+            <span className="h-[5px] w-full bg-red-500"/>
+            <p className="text-[12px] text-justify">{item.desc}</p>
+          </div>
+        ))}
+      </div>
+      {/* ==================== */}
     </div>
   );
 }
